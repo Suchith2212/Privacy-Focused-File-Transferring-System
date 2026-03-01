faa38daee97d0309e247fe7ac5c29b3e006476fd
Alluri Yashwanth Sanjay Varma <sanjayalluri4545@gmail.com>
2026-03-01 12:46:37 +0530

    Updated db.js

--- BlindDrop-mvp/backend/db.js
+++ BlindDrop-mvp/backend/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Sanjay31766@",
+  password: "password",
   database: "BlindDrop",
 });
 
